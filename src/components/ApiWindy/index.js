81a0af20a4c52e626c3c449c79d0ce376811e9a1
yanndemor <yann.demor@gmail.com>
2021-07-07 15:45:49 +0200

    more content on the apiwindy components

--- src/components/ApiWindy/index.js
+++ src/components/ApiWindy/index.js
@@ -8,6 +8,9 @@ import PropTypes from 'prop-types';
 import { slugifyTitle } from 'src/utils';
 import './apiwindy.scss';
 import { Card, Image } from 'semantic-ui-react';
+
+import guillemet1 from 'src/assets/images/guillemets 1.png';
+import guillemet2 from 'src/assets/images/guillemets 2.png';
 // == Composant
 const ApiWindy = ({ loadCategory, camList }) => {
   useEffect(() => {
@@ -15,21 +18,36 @@ const ApiWindy = ({ loadCategory, camList }) => {
   }, []); console.log(camList);
   return (
     <>
-    <div className="container-title">
-     <div>
+    <div className="container-presentation-windy">
+     <div className="container-title">
         <h1>Api Windy Webcam</h1> 
      </div> 
+     <div className="block-description">
+          <div className="windy-guillemet1">
+              <img  src={guillemet1} alt="guillemets" />
+          </div>
+          <div className="description-api">
+            <h3>Reach one of the following webcams listed below</h3>
+          </div>   
+          <div className="windy-guillemet2">
+            <img src={guillemet2} alt="guillemets" />
+          </div>
+      </div>
+      <div className="description-windy">
+        <h4 className="description">Description:</h4>
+        <p className="paragraphe-description">In this project, I have used an API request on <a href="https://api.windy.com/" className="link">WindyApi</a>, permitting to collect a list of webcams available all around the world, I have selected a few one présented here <strong>click on the webcam's name</strong> to watch it!
+        </p>
+        <p> This projet is produced using the library javascript React and
+          Redux to manage the request API and the datas to the differents React's components!
+        </p>
+
+    </div>
     </div>
     <div className="container-cam">
      
-     
-       
      <Card.Group id="card-row" itemsPerRow={2} >  
-      {
-        camList.map((cam)=>(
-       
-        
-          
+      {camList.map((cam)=>(
+
             <Card id="container-card" key={cam.id}>
             
               <Image src={cam.image.current.thumbnail} wrapped ui={false} />
--- src/components/Cam/index.js
+++ src/components/Cam/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Iframe from 'react-iframe';
 import { useParams } from 'react-router-dom';
+import { Eye, Info } from 'react-feather';
 
 // == Import : local
 import { getCamBySlug } from 'src/utils';
-
+import './cam.scss';
 const Cam = ({ camList }) => {
  console.log(camList);
   const { slug } = useParams();
@@ -13,15 +14,31 @@ const Cam = ({ camList }) => {
   const cam = getCamBySlug(slug, camList);
 console.log(cam);
   return (
-    <div>
-      <Iframe url={cam.player.day.embed}
-        width="450px"
-        height="450px"
-        id={cam.id}
-        className="myClassname"
-       
-      />
-    </div>
+    <>
+      <div className="container-title">
+        <h1>Webcam {cam.title}</h1> 
+     </div> 
+      <div className="message">
+        <div id="alert">
+          <Eye width="50" height="50"/>
+          </div>
+        <div className="info">
+          <p className="text"> You can access the live cam using the <strong>source link </strong> provider by the <Info 
+          fill="grey" 
+         /> icon !!!</p>
+        </div>
+      </div>
+     
+      <div className="container-singlecam">
+        <Iframe url={cam.player.day.embed}
+          width="600px"
+          height="600px"
+          id={cam.id}
+          className="myClassname"
+        
+        />
+      </div>
+    </>
   );
 };
 export default Cam;
